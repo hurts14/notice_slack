2032f651705f201878cf3fa7df5fa06896bf5bae
hurts14 <bayern.uver@gmail.com>
2020-05-23 15:53:27 +0900

    add gspread done

--- scr/scraping.py
+++ scr/scraping.py
@@ -9,9 +9,11 @@ from oauth2client.service_account import ServiceAccountCredentials
 SCOPE = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 #認証情報設定
 #ダウンロードしたjsonファイル名をクレデンシャル変数に設定（秘密鍵、Pythonファイルから読み込みしやすい位置に置く）
-CREDENTIALS = ServiceAccountCredentials.from_json_keyfile_name('receive-vix-c7b393541993.json', SCOPE)
+json_path = 'receive-vix-c7b393541993.json'
+CREDENTIALS = ServiceAccountCredentials.from_json_keyfile_name(json_path, SCOPE)
 
 NOW = datetime.datetime.now().strftime("%Y-%m-%d")
+
 
 def get_google_spread(date):
     #OAuth2の資格情報を使用してGoogle APIにログインします。
@@ -22,18 +24,18 @@ def get_google_spread(date):
 
     #共有設定したスプレッドシートのシート1を開く
     worksheet = gc.open_by_key(SPREADSHEET_KEY).sheet1
+    #import_value = int(worksheet.acell('A1').value)
+    #dateの行の値を受け取る
+    #指定した文字列を検索してマッチした全てのセルを取得 
+    cell = worksheet.find(str(date))
+    value_list = worksheet.row_values(cell.row)
 
-    #A1セルの値を受け取る
-    import_value = int(worksheet.acell('A1').value)
-
-    #A1セルの値に100加算した値をB1セルに表示させる
-    export_value = import_value+100
-    worksheet.update_cell(1,2, export_value)
     #dateのvixとnikkeiの値を受け取る
-    vix = ''
-    nikkei = ''
+    vix = int(value_list[2])
+    nikkei = float(value_list[1])
 
     return vix, nikkei
+
 
 def set_vix(vix, nikkei):
 
@@ -41,14 +43,13 @@ def set_vix(vix, nikkei):
     SPREADSHEET_KEY = '14kv6zXOJUoGJev7UzRekhKO0wuvLQR6vlsS0n9x42m4'
     worksheet = gc.open_by_key(SPREADSHEET_KEY).sheet1
     
-    #A1セルの値を受け取る
-    import_value = int(worksheet.acell('A1').value)
+    #行数を取得=最終行
+    #all_len = len(worksheet.get_all_values())
+    update_list = [NOW, nikkei, vix]
 
-    #A1セルの値に100加算した値をB1セルに表示させる
-    export_value = import_value+100
-    worksheet.update_cell(1,2, export_value)
-    #dateセルにvixとnikkeiのを表示させる
-
+    #spreadの一番最後の行にdateとvixとnikkeiの値を表示させる
+    worksheet.append_row(update_list)
+    #worksheet.update_cell(1,2, export_value)
 
 
 def request_nikkei():
@@ -57,14 +58,19 @@ def request_nikkei():
     print('nikkei')
     return r
 
+
 def request_vix():
     vix_url = 'https://indexes.nikkei.co.jp/nkave/historical/nikkei_stock_average_vi_daily_jp.csv'
     r = requests.get(vix_url)
     print('vix')
     return r
 
+
 def main():
     print('hello')
+    #set_vix(5,10)
+    #v, n = get_google_spread(NOW)
+    #print('{},{}'.format(v,n))
 
 if __name__ == "__main__":
     main()
